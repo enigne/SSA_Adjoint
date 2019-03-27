97de7c3850785b6cb83696db985b7a88dfe34f98
Cheng Gong <cheng.gong@it.uu.se>
2019-03-27 11:56:21 +0100

    change the computational domain to grounded part only

--- adjointSSA.m
+++ adjointSSA.m
@@ -7,11 +7,12 @@ load('DATA/SSAinit_N400.mat')
 saveFlag = 1;
 %% Setup restart
 ist = [1:260];
-N_restart = 10;
+N_restart = 1;
 transientFlag = 1;
 H_mat = zeros(length(H), N_restart);
 u_mat = zeros(length(u), N_restart);
-dt = 0.1;
+gpos_vec = zeros(N_restart,1);
+dt = 1;
 
 Nist = length(ist);
 psi_mat = zeros(N+1, Nist, N_restart);
@@ -24,67 +25,62 @@ for i = 1: N_restart
     [glInd, H, u, beta]=FlowlineSSA(H, b, x, dx, Nx, A, C, m, n, rhoi, rhow, g, as, dt, dt, u);
     H_mat(:, i) = H;
     u_mat(:, i) = u;
+    gpos_vec(i) = glInd;
 end
 %% For adjSSA you need the input
 rhoig = rhoi*g;
-sigma= 0.5e4;
+sigma= 0.5e2;
 n=3;
-x = x(2:end);
-Nx = length(x)+1;
-epsilon = 1000;
 
-%% Solve steady states adjoint SSA
-psi_old = zeros(Nx-1, 1);
-I = eye(Nx-1);
-% get the forward solutions
-u = u(2:end);
-% H on stagger grid
-H = (H(1:end-1)+H(2:end)) * 0.5;
+%% Solve backward in time
+psi_old = zeros(N+1, 1);
+% Artificial viscosity
+epsilon = 1e4;
+
 for i =  1:Nist
     for t =  N_restart:-1:1
+        % get the forward solutions
+        u = u_mat(:,t);
+        H = H_mat(:,t);
+        glInd = gpos_vec(t);
+        % set for adjoint
+        xAdj = x(2:glInd);
+        Nx = length(xAdj)+1;
+        I = eye(Nx-1);
+        
+        u = u(2:glInd);
+        % H on stagger grid
+        H = (H(1:glInd-1)+H(2:glInd)) * 0.5;
+        % construct Adjoint matrices
+        [A11, A12, A21, A22, F1, F2, ux, eta]=constrauctAdjSSAMatrices(Nx-1,n,ist(i),sigma,u,H,mean(bxc),A,rhoig,dx,glInd,epsilon);
+        % Time stepping
+        Q = [A11 - transientFlag*1./dt .* I,	A12;
+            A21,                                A22;];
+        rhs = [F1 - transientFlag*1./dt .* psi_old(1:glInd-1);
+            F2;];
+        
+        psifi = Q\rhs;
+        
+        psi = psifi(1:Nx-1);
+        phi = psifi(Nx:2*Nx-2);
 
-    % construct Adjoint matrices
-    [A11, A12, A21, A22, F1, F2, ux, eta]=constrauctAdjSSAMatrices(Nx-1,n,ist(i),sigma,u,H,mean(bxc),A,rhoig,dx,glInd,epsilon);
-    % Time stepping
-    Q = [A11 - transientFlag*1./dt .* I,	A12;
-        A21,                                A22;];
-    rhs = [F1 - transientFlag*1./dt .* psi_old;
-        F2;];
-    
-    psifi = Q\rhs;
-    
-    psi = psifi(1:Nx-1);
-    phi = psifi(Nx:2*Nx-2);
-    wght = -phi .* u.^m;
-    bwght = (Dp(Nx-1, dx)*psi).*u + (Dcd(Nx-1,dx)* phi) .*eta .* ux+ rhoig*phi.*(Dcd(Nx-1,dx)*H + bxc);
-    
-    psi_mat(:, i, t) = psi;
-    phi_mat(:, i, t) = phi;
-    wght_mat(:, i, t) = wght;
-    bwght_mat(:, i, t) = bwght;
+        phi(ist(i)+5:end)=0;
+
+        wght = -phi .* u.^m;
+        bwght = (Dp(Nx-1, dx)*psi).*u + (Dcd(Nx-1,dx)* phi) .*eta .* ux+ rhoig*phi.*(Dcd(Nx-1,dx)*H + bxc(1:Nx-1));
+        
+        psi_mat(1:glInd-1, i, t) = psi;
+        phi_mat(1:glInd-1, i, t) = phi;
+        wght_mat(1:glInd-1, i, t) = wght;
+        bwght_mat(1:glInd-1, i, t) = bwght;
     end
 end
 
-% %%
-% figure
-% subplot(2,2,1)
-% plot(psi_mat)
-% ylabel('$\psi$', 'Interpreter','latex')
-% subplot(2,2,2)
-% plot(phi_mat)
-% ylabel('$\phi$', 'Interpreter','latex')
-% subplot(2,2,3)
-% plot(wght_mat)
-% ylabel('$\delta C$ sensitivity', 'Interpreter','latex')
-% subplot(2,2,4)
-% plot(bwght_mat)
-% ylabel('$\delta b$ sensitivity', 'Interpreter','latex')
-
 %%
-if saveFlag 
-if transientFlag
-    save(['DATA/SSAAdjointTransient_T', num2str(N_restart) ,'_N', num2str(N) ,'.mat'], 'x', 'ist', 'psi_mat', 'phi_mat', 'wght_mat', 'bwght_mat');
-else
-    save(['DATA/SSAAdjoint_N', num2str(N) ,'.mat'], 'x', 'ist', 'psi_mat', 'phi_mat', 'wght_mat', 'bwght_mat');
-end
+if saveFlag
+    if transientFlag
+        save(['DATA/SSAAdjointTransient_T', num2str(N_restart) ,'_N', num2str(N) ,'.mat'], 'x', 'ist', 'psi_mat', 'phi_mat', 'wght_mat', 'bwght_mat');
+    else
+        save(['DATA/SSAAdjoint_N', num2str(N) ,'.mat'], 'x', 'ist', 'psi_mat', 'phi_mat', 'wght_mat', 'bwght_mat');
+    end
 end
--- testSSA.m
+++ testSSA.m
@@ -4,14 +4,12 @@ close all
 addpath('SSA')
 %% Load final H and u from init file
 load('DATA/SSAinit_N800.mat')
-
 %% Setup restart
 N_restart = 10;
 H_mat = zeros(length(H), N_restart);
 u_mat = zeros(length(u), N_restart);
 gpos_vec = zeros(1, N_restart);
 dt = 0.1;
-
 %% Solve SSA GL problem
 for i = 1: N_restart
     [glInd, H, u, beta]=FlowlineSSA(H, b, x, dx, Nx, A, C, m, n, rhoi, rhow, g, as, dt, dt, u);
@@ -21,67 +19,69 @@ for i = 1: N_restart
 end
 %% For adjSSA you need the input
 rhoig = rhoi*g;
-sigma= 0.5e2; 
-ist = 150;
+sigma= 0.5e4; 
+ist = 100;
 n=3;
-x = x(2:end);
-Nx = length(x)+1;
-Ascal = 1e-6;
-% beta = beta(2:end);
 
 %% Solve backward in time
-psi_old = zeros(Nx-1, 1);
-I = eye(Nx-1);
+psi_old = zeros(N+1, 1);
+phi_old = zeros(N+1, 1);
 % Artificial viscosity
-epsilon = 0;
-
+epsilon = 1e3;
+Ascale = 1;
 for i =  N_restart:-1:1
     % get the forward solutions
     u = u_mat(:,i);
     H = H_mat(:,i);
-    u = u(2:end);
+    glInd = gpos_vec(i);
+    % set for adjoint
+    xAdj = x(2:glInd);
+    Nx = length(xAdj)+1;
+    I = eye(Nx-1);
+
+    u = u(2:glInd);
     % H on stagger grid
-    H = (H(1:end-1)+H(2:end)) * 0.5;
+    H = (H(1:glInd-1)+H(2:glInd)) * 0.5;
     % construct Adjoint matrices
 %     [A11, A12, A21, A22, F1, F2]=constrauctAdjSSA(Nx-1,n,ist,sigma,u,H,mean(bxc),A,rhoig,dx);
     [A11, A12, A21, A22, F1, F2, ux, eta, beta]=constrauctAdjSSAMatrices(Nx-1,n,ist,sigma,u,H,mean(bxc),A,rhoig,dx, glInd, epsilon);
     % Time stepping
-    Q = [A11 - 1./dt .* I,	A12;
-        A21,                A22;];
-    rhs = [F1 - 1./dt .* psi_old;
+    Q = [A11*Ascale - Ascale./dt .* I,	A12;
+        A21*Ascale,                A22;];
+    rhs = [F1 - Ascale./dt .* psi_old(1:glInd-1);
         F2;];
     
     psifi = Q\rhs;
-    psi_old = psifi(1:Nx-1);
+    psi_old = psifi(1:Nx-1)/Ascale;
     fi_old = psifi(Nx:2*Nx-2);    
-    
-    psi_old(end) = 0;
-    
+   
+    fi_old(ist+10:end)=0;
     wght = -fi_old .* u.^m;
     
-    figure(1)
+    figure(2)
     subplot(3,1,1)
-    plot(x, psi_old)
+    plot(xAdj, psi_old)
     ylabel('$\psi$', 'Interpreter','latex')
     subplot(3,1,2)
-    plot(x, fi_old)
+    plot(xAdj, fi_old)
     ylabel('$\phi$', 'Interpreter','latex')
     subplot(3,1,3)
-    plot(x, wght)
+    plot(xAdj, wght)
     ylabel('$\delta\beta$', 'Interpreter','latex')
 end
 
 %%
-pert = sum(wght.*C(1:end-1)*0.01)*dx;
+pert = sum(wght.*C(1:glInd-1)*0.01)*dx;
 
 %%
 figure
 t1 = Dcd(Nx-1,dx)*(1./n.*H.*eta.* (Dcd(Nx-1,dx)*fi_old));
 t2 =-m*beta.*fi_old;
 t3 = -H.*(Dcd(Nx-1,dx)*psi_old);
-% subplot(3,1,1)
+subplot(3,1,1)
+plot(t1)
+subplot(3,1,2)
+plot(t2)
+subplot(3,1,3)
 plot(t1+t2+t3)
 
-% hold on
-% plot(F2)
-% plot(A21*psi_old+A22*fi_old -F2)
