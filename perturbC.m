250c4dd37406cb3f0a4d2d9fbdca15ea0dcd6959
Cheng Gong <cheng.gong@it.uu.se>
2019-03-27 22:12:41 +0100

    make forward solution flexible when generate data files.

--- perturbC.m
+++ perturbC.m
@@ -2,13 +2,16 @@ clear
 close all
 %%
 addpath('SSA')
+N = 800;
 %% Load final H and u from init file
-load('DATA/SSAinit_N1600.mat')
+load(['DATA/SSAinit_N', num2str(N), '.mat'])
 saveFlag = 1;
 
 %%
 pertubation = 0.01;
-dC = pertubation.*C.*((x>5.5e5)&(x<7.5e5));
+lWin = 900e3;
+rWin = 1000e3;
+dC = pertubation.*C.*((x>lWin)&(x<rWin));
 
 %% Setup restart
 N_restart = 20000;
@@ -16,8 +19,8 @@ H_mat = zeros(length(H), N_restart);
 u_mat = zeros(length(u), N_restart);
 gpos_vec = zeros(1, N_restart);
 dt = 1;
-u_ref=u;
-
+u_ref= u;
+H_ref = H;
 %% Solve SSA GL problem
 for i = 1: N_restart
     [gpos, H, u, beta]=FlowlineSSA(H, b, x, dx, Nx, A, C+dC, m, n, rhoi, rhow, g, as, dt, dt, u);
@@ -27,8 +30,8 @@ for i = 1: N_restart
 end
 
 %%
-plot(x, u_ref-u_mat(:,end))
+plot(x, u_mat(:,end) -u_ref)
 %%
 if saveFlag 
-    save(['DATA/SSAPerturb_N', num2str(N),'_C' , num2str(pertubation*100,'%03.f') ,'.mat'], 'x','u_mat','H_mat','u_ref', 'dt','N_restart','pertubation', 'dC');
+    save(['DATA/SSAPerturb_N', num2str(N),'_C' , num2str(pertubation*100,'%03.f'), '_x', num2str(lWin/1000) ,'.mat'], 'x','u_mat','H_mat','u_ref','H_ref', 'dt','N_restart','pertubation', 'dC');
 end
