c7388c1f12e1287de06e1e8dbc757da43e374691
Cheng Gong <cheng.gong@it.uu.se>
2019-03-27 21:07:37 +0100

    clean up initialization

--- initSSA.m
+++ initSSA.m
@@ -11,14 +11,13 @@ dx = L/(N);
 x = linspace(-dx,L,Nx)';
 
 %%
-b =  -(-720 +778.5*(x/7.5e5) );
+b =  -(-720 +778.5*(x/7.5e5));
 dt = 1;
 T = 30000;
 
 %% symmetric ice divide (extra grid point)
 b(1)=b(3);
 bxc = diff(b)/dx;
-
 H=zeros(Nx,1)+10;
 
 %% Constants
@@ -41,26 +40,8 @@ A = A*secperyear;
 
 %% Solve SSA GL problem
 [GLpos, H, u, beta]=FlowlineSSA(H, b, x, dx, Nx, A, C, m, n, rhoi, rhow, g, as, dt, T);
-% u = u(2:end);
-% % H on stagger grid
-% H = (H(1:end-1)+H(2:end)) * 0.5;
-% beta = beta(2:end);
 
 %% Save final H and u to mat file
 if saveFlag 
     save(['DATA/SSAinit_N', num2str(N) ,'.mat'])
 end
-%%
-% %%
-% x0=0; 
-% xN=1.8e6; 
-% h=dx; 
-% rhoig=9.8*900; 
-% AA=1.4648e-17; 
-% bxc=constant b_x; 
-% sigma=0.5e4; 
-% xst=x(ist); 
-% n=3; 
-% iGL=123;
-% 
-% [psi,fi,wght,bwght,A,f,fic,eta,beta,fix,psix,hx,ux,B,G,K]=adjssa(N,n,ist,xst,sigma,u,H,betain,bxc,AA,rhoig,h,x0,xN);
