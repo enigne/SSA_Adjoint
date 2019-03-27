a305329ed71f890a44c51db33214a5b57b96e4d9
Cheng Gong <cheng.gong@it.uu.se>
2019-03-27 21:21:52 +0100

    add opitions to choose u or H observation

--- constrauctAdjSSAMatrices.m
+++ constrauctAdjSSAMatrices.m
@@ -1,4 +1,4 @@
-function [A11, A12, A21, A22, F1, F2, ux, eta, beta]=constrauctAdjSSAMatrices(N, n, ist, sigma, u, H, bxc, Afact, rhoig, dx, glInd, epsilon)
+function [A11, A12, A21, A22, F1, F2, ux, eta, beta]=constrauctAdjSSAMatrices(N, n, ist, sigma, u, H, bxc, Afact, rhoig, dx, uObs, HObs, glInd, epsilon)
 % solve for the adjoint of the SSA equation
 % Input
 % N=number of inner points, boundaries at 0 and N+1
@@ -22,19 +22,12 @@ Afact=2/Afact^(1/n);
 m=1/3;
 % m=1;
 mm1=m-1;
-gamc=0;
-gamc1=0;
-gamc2=0;
 ux=zeros(N,1);
 eta=ux;
 beta=ux;
 fic=ux;
 bx=ux;
-gam=ux;
-gam1=gam;
-gam2=gam;
-Fh=ux;
-Fu=ux;
+F = ux;
 %
 p=(1-n)/n;
 N1=N-1;
@@ -45,22 +38,28 @@ for i=2:N1
 end
 ux(1)=ux(2);
 ux(N)=ux(N1);
-% artificial damping
-Ngam=ceil(0.99*N);
-for i=Ngam:N
-    gam(i)=gamc*(i-Ngam)/(N-Ngam);
-    gam1(i)=gamc1*(i-Ngam)/(N-Ngam);
-    gam2(i)=gamc2*(i-Ngam)/(N-Ngam);
-end
 
+% gaussian for the observation
 gaussfact=1/sqrt(2*pi*sigma^2);
 xi = zeros(N, 1);
 for i=1:N
     xi(i)=(i-ist)*dx;
-    Fu(i)=gaussfact*exp(-xi(i)^2/(2*sigma^2));
+    F(i)=gaussfact*exp(-xi(i)^2/(2*sigma^2));
 end
-Fint = dx*sum(Fu);
-Fu=Fu./Fint;
+Fint = dx*sum(F);
+F = F./Fint;
+% check which observation function
+if uObs
+    Fu = F;
+else
+    Fu = 0*F;
+end
+if HObs
+    Fh = F;
+else
+    Fh = 0*F;
+end
+
 % viscosity, fi coeff in first eq, bottom derivative
 for i=1:N
     eta(i)=Afact*abs(ux(i))^p;
@@ -78,7 +77,7 @@ end
 % D2(N,dx)
 visco = zeros(N,1);
 for i = 1:N
-    if abs(i - glInd) >=0
+    if abs(i - glInd) < 50
         visco(i) = epsilon;
     end
 end
@@ -104,7 +103,7 @@ F1(N) = 0;
 
 % A21(1, :) = 0;
 % A21(N, :) = 0;
-% 
+%
 A22(1, :) = 0;
 A22(N, :) = 0;
 A22(1, 1) = A22(2, 2);
--- testSSA.m
+++ testSSA.m
@@ -1,15 +1,19 @@
 clear
-close all
+% close all
 %%
 addpath('SSA')
 %% Load final H and u from init file
-load('DATA/SSAinit_N800.mat')
+load('DATA/SSAinit_N1600.mat')
 %% Setup restart
-N_restart = 10;
+N_restart = 1;
+uObs = 1;
+HObs = 1 - uObs;
+transientFlag = 1;
+dt = 1;
+%% Initialization
 H_mat = zeros(length(H), N_restart);
 u_mat = zeros(length(u), N_restart);
 gpos_vec = zeros(1, N_restart);
-dt = 0.1;
 %% Solve SSA GL problem
 for i = 1: N_restart
     [glInd, H, u, beta]=FlowlineSSA(H, b, x, dx, Nx, A, C, m, n, rhoi, rhow, g, as, dt, dt, u);
@@ -19,15 +23,15 @@ for i = 1: N_restart
 end
 %% For adjSSA you need the input
 rhoig = rhoi*g;
-sigma= 0.5e4; 
-ist = 100;
+sigma= 5e3; 
+ist = 200;
 n=3;
 
 %% Solve backward in time
 psi_old = zeros(N+1, 1);
 phi_old = zeros(N+1, 1);
 % Artificial viscosity
-epsilon = 1e3;
+epsilon = 1e4;
 Ascale = 1;
 for i =  N_restart:-1:1
     % get the forward solutions
@@ -44,26 +48,27 @@ for i =  N_restart:-1:1
     H = (H(1:glInd-1)+H(2:glInd)) * 0.5;
     % construct Adjoint matrices
 %     [A11, A12, A21, A22, F1, F2]=constrauctAdjSSA(Nx-1,n,ist,sigma,u,H,mean(bxc),A,rhoig,dx);
-    [A11, A12, A21, A22, F1, F2, ux, eta, beta]=constrauctAdjSSAMatrices(Nx-1,n,ist,sigma,u,H,mean(bxc),A,rhoig,dx, glInd, epsilon);
+    [A11, A12, A21, A22, F1, F2, ux, eta, beta]=constrauctAdjSSAMatrices(...
+        Nx-1,n,ist,sigma,u,H,mean(bxc),A,rhoig,dx, uObs, HObs, glInd, epsilon);
     % Time stepping
-    Q = [A11*Ascale - Ascale./dt .* I,	A12;
+    Q = [A11*Ascale - transientFlag*Ascale./dt .* I,	A12;
         A21*Ascale,                A22;];
-    rhs = [F1 - Ascale./dt .* psi_old(1:glInd-1);
+    rhs = [F1 - transientFlag*Ascale./dt .* psi_old(1:glInd-1);
         F2;];
-    
+
     psifi = Q\rhs;
     psi_old = psifi(1:Nx-1)/Ascale;
-    fi_old = psifi(Nx:2*Nx-2);    
+    phi_old = psifi(Nx:2*Nx-2);    
    
-    fi_old(ist+10:end)=0;
-    wght = -fi_old .* u.^m;
+%     phi_old(ist-2:end)=0;
+    wght = -phi_old .* u.^m;
     
-    figure(2)
+    figure(1)
     subplot(3,1,1)
     plot(xAdj, psi_old)
     ylabel('$\psi$', 'Interpreter','latex')
     subplot(3,1,2)
-    plot(xAdj, fi_old)
+    plot(xAdj, phi_old)
     ylabel('$\phi$', 'Interpreter','latex')
     subplot(3,1,3)
     plot(xAdj, wght)
@@ -75,13 +80,15 @@ pert = sum(wght.*C(1:glInd-1)*0.01)*dx;
 
 %%
 figure
-t1 = Dcd(Nx-1,dx)*(1./n.*H.*eta.* (Dcd(Nx-1,dx)*fi_old));
-t2 =-m*beta.*fi_old;
+t1 = Dcd(Nx-1,dx)*(1./n.*H.*eta.* (Dcd(Nx-1,dx)*phi_old));
+t2 =-m*beta.*phi_old;
 t3 = -H.*(Dcd(Nx-1,dx)*psi_old);
 subplot(3,1,1)
 plot(t1)
 subplot(3,1,2)
 plot(t2)
 subplot(3,1,3)
+plot(t3)
+%%
+figure
 plot(t1+t2+t3)
-
