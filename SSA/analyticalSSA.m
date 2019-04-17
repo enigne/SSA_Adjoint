ed5bb525c6be83d22e31919b1e4e1fa85511bb12
Cheng Gong <cheng.gong@it.uu.se>
2019-04-17 18:57:52 +0200

    change the analytical solver according to the correction for steady state.

--- SSA/analyticalSSA.m
+++ SSA/analyticalSSA.m
@@ -1,6 +1,7 @@
 function [x, H, u, psi, v, wght, bwght] = analyticalSSA(H0, HGL, m, C, a, rhog, x, iGL, ist)
 %% prepare for \int_x^x* Cx^{m-1} dx
-GLmask = (x < x(iGL+1));
+xGL = x(iGL+1);
+GLmask = (x < xGL);
 integrant = C.*x.^m .* GLmask;
 Hfact = trapz(x, integrant) - cumtrapz(x, integrant);
 
@@ -13,20 +14,20 @@ u = a*x./H;
 %% psi
 xst = x(ist+1);
 Hst = H(ist+1);
-mask = (x > xst);
+mask = (x < xst);
 
 fic = xst/(rhog*Hst^(m+3));
-axstm = (a*xst)^m;
-psi = -1/Hst+C.*fic.*((a.*x).^m-axstm);
+axstm = (a*xGL)^m;
+psi =  C.*fic.*(axstm - (a.*x).^m);
+% 
+% integrant = C.*x.^(m-1).*GLmask;
+% psifact =  trapz(x, integrant) - cumtrapz(x, integrant);
+% psi = m*a^(m+1).* fic.*psifact;
 
-% integrant = C.*x.^(m-1) .* (1-mask);
-% psifact = trapz(x, integrant) - cumtrapz(x, integrant);
-% psi = -1/Hst - m*a^(m+1).* fic.*psifact;
-
-psi(mask) = 0;
+psi(mask) = -1/Hst + psi(ist+1);
 
 %% v
-v = -a*fic*H.^m;
+v = a*fic*H.^m;
 v(mask) = 0;
 %% C weights
 wght =- v .* u.^m;
