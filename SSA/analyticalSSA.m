81a3341858ccd21cd6803f90a86a259989864cb7
Cheng Gong <cheng.gong@it.uu.se>
2019-05-07 09:30:53 +0200

    add analytical solutions for h response

--- SSA/analyticalSSA.m
+++ SSA/analyticalSSA.m
@@ -1,9 +1,10 @@
-function [x, H, u, psi, v, wght, bwght] = analyticalSSA(H0, HGL, m, C, a, rhog, x, iGL, ist)
+function [x, H, u, psi_u, v_u, wght_u, bwght_u, psi_h, v_h, wght_h, bwght_h] = analyticalSSA(H0, HGL, m, C, a, rhog, x, iGL, ist)
 %% prepare for \int_x^x* Cx^{m-1} dx
 xGL = x(iGL+1);
 GLmask = (x < xGL);
 integrant = C.*x.^m .* GLmask;
 Hfact = trapz(x, integrant) - cumtrapz(x, integrant);
+dx = abs(x(2) - x(1));
 
 %% Compute for H
 H =  (HGL^(m+2)  + (m+2)*a^m/rhog * Hfact).^(1/(m+2));
@@ -11,31 +12,54 @@ H =  (HGL^(m+2)  + (m+2)*a^m/rhog * Hfact).^(1/(m+2));
 %% u
 u = a*x./H;
 
-%% psi
+%% Masks
 xst = x(ist+1);
 Hst = H(ist+1);
-mask = (x < xst);
+maskST = (x < xst);
+maskGL = (x > xGL);
+mask = maskST | maskGL;
 
-fic = xst/(rhog*Hst^(m+3));
-axstm = (a*xGL)^m;
-psi =  C.*fic.*(axstm - (a.*x).^m);
 % 
-% integrant = C.*x.^(m-1).*GLmask;
-% psifact =  trapz(x, integrant) - cumtrapz(x, integrant);
-% psi = m*a^(m+1).* fic.*psifact;
-
-psi(mask) = -1/Hst + psi(ist+1);
+integrant = C.*x.^(m-1).*GLmask;
+psifact =  trapz(x, integrant) - cumtrapz(x, integrant);
 
 %% v
-v = a*fic*H.^m;
-v(mask) = 0;
+Cv = a/rhog.*xst./Hst.^(m+3);
+v_u = Cv.*H.^m;
+v_u(mask) = 0;
+
+%% psi
+psi_u = Cv .* m .* a^(m-1) .* psifact;
+psi_u(maskST) = -1/Hst + psi_u(ist+1);
+psi_u(maskGL) = 0;
+
 %% C weights
-wght =- v .* u.^m;
+wght_u =- v_u .* u.^m;
 
 %% b weights
-% bwght = (m+1)*C*a*xst.*(a*x).^m./(rhog*Hst^(m+3).*H);
 Hx = -C.*a.^m/rhog.*x.^m./H.^(m+1);
-psix = -C.*m.*u.^(m-1).*v./H;
-psix(ist+1) = 1/Hst/1e3;
-bwght = psix.*u+v.*rhog.*Hx;
+vx = m*Cv.*H.^(m-1).*Hx;
+vx(maskST) = 0;
+vx(ist+1) = u(ist)/rhog/Hst^2/dx;
+bwght_u = rhog*(vx.*H + Hx.*v_u);
+%% for h-response
+%% v
+Cv_h = -1./rhog/(Hst^(m+1));
+v_h = Cv_h.*H.^m;
+v_h(mask) = 0;
 
+%% psi
+psi_h =  Cv_h .* m .* a^(m-1) .* psifact;
+psi_h(maskST) = psi_h(ist+1);
+psi_h(maskGL) = 0;
+
+%% C weights
+wght_h =- v_h .* u.^m;
+
+%% b weights
+Hx = -C.*a.^m/rhog.*x.^m./H.^(m+1);
+vx_h = m*Cv_h.*H.^(m-1).*Hx;
+vx_h(maskST) = 0;
+vx_h(ist+1) = -1./rhog/Hst/dx;
+bwght_h = rhog*(vx_h.*H + Hx.*v_h);
+bwght_h(ist+1) = bwght_h(ist+1) + 1/dx;
--- compareAna.m
+++ compareAna.m
@@ -1,5 +1,5 @@
 clear
-close all
+% close all
 %%
 addpath('SSA')
 %% Load final H and u from init file
@@ -9,13 +9,13 @@ load('DATA/SSAinit_N1600.mat')
 C = beta;
 m = 1;
 %% Compute analytical solutions
-saveFlag = 0;
+saveFlag = 1;
 iGL = GLpos;
 slicePos = 700;
 % [u_ana,H_ana,beta_ana,x_ana,phi_ana,psi_ana,wght_ana,bwght_ana,dudC]=...
 %     Hum(N,L,H(2),H(iGL+1),iGL,slicePos,C,rhog,as,1.00, m);
 
-[x_ana, H_ana, u_ana, psi_ana, phi_ana, wght_ana, bwght_ana] = ...
+[x_ana, H_ana, u_ana, psi_ana, phi_ana, wght_ana, bwght_ana, psi_ana_h, phi_ana_h, wght_ana_h, bwght_ana_h] = ...
     analyticalSSA(H(2), H(iGL+1), m, C(2:end-1), as, rhog, x(2:end-1), iGL, slicePos);
 %% plot
 figure
@@ -33,11 +33,21 @@ plot(x_ana, psi_ana)
 subplot(4,1,2)
 plot(x_ana, phi_ana)
 subplot(4,1,3)
-plot(x_ana,wght_ana) 
+plot(x_ana,psi_ana_h) 
 subplot(4,1,4)
-plot(x_ana,bwght_ana) 
+plot(x_ana,phi_ana_h) 
+figure
+subplot(4,1,1)
+plot(x_ana, wght_ana)
+subplot(4,1,2)
+plot(x_ana, bwght_ana)
+subplot(4,1,3)
+plot(x_ana,wght_ana_h) 
+subplot(4,1,4)
+plot(x_ana,bwght_ana_h) 
 %% save
 if saveFlag
     save(['DATA/SSAAnalytical_N', num2str(N), '.mat'], ...
-         'x_ana', 'u_ana', 'H_ana', 'phi_ana', 'psi_ana', 'wght_ana', 'bwght_ana', 'slicePos');
+         'x_ana', 'u_ana', 'H_ana', 'phi_ana', 'psi_ana', 'wght_ana', 'bwght_ana', ...
+         'phi_ana_h', 'psi_ana_h', 'wght_ana_h', 'bwght_ana_h', 'slicePos');
 end
