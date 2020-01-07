7652eb5c04383e392fc7b80347badf11e83f733d
Cheng Gong <cheng.gong@it.uu.se>
2020-01-07 17:17:25 +0100

    add time dependent adjoint

--- testSSA.m
+++ testSSA.m
@@ -2,147 +2,247 @@ clear
 close all
 %%
 addpath('SSA')
+N = 800;
+
 %% Load final H and u from init file
-load('DATA/SSAinit_N1600.mat')
+load(['DATA/SSAinit_N', num2str(N), '.mat'])
+saveFlag = 1;
+
 %% Setup restart
-N_restart = 1;
-uObs = 0;
+uObs = 1;
 HObs = 1 - uObs;
-transientFlag = 0;
-dt = 1;
+transientFlag = 1;
+MacayealFalg = 0;
+
+%% For adjSSA you need the input
+rhoig = rhoi*g;
+sigma= 1e2;
+ist = [450];
+n = 3;
+
+%% Pertubation in time
+N_restart = 400;
+dt = 0.05;
+sInd = [0: (N_restart-1)];
+seasonType = 0;
+
+if seasonType == 0
+    % sine [-1,1]
+    season = sin(sInd*dt*2*pi);
+elseif seasonType == 1
+    % cosine [-1,1]
+    season = cos(sInd*dt*2*pi);
+else
+    % square [0, 1]
+    season = (sin(sInd*dt*2*pi) > 0)*2-1;
+end
+
 %% Initialization
+Nist = length(ist);
+
 H_mat = zeros(length(H), N_restart);
 u_mat = zeros(length(u), N_restart);
-gpos_vec = zeros(1, N_restart);
+beta_mat = zeros(length(C), N_restart);
+gpos_vec = zeros(N_restart,1);
+
+psi_mat = zeros(N+1, N_restart, Nist);
+phi_mat = zeros(N+1, N_restart, Nist);
+wght_mat = zeros(N+1, N_restart, Nist);
+bwght_mat = zeros(N+1, N_restart, Nist);
+
+v_mat = zeros(N+1, N_restart, Nist);
+vWght_mat = zeros(N+1, N_restart, Nist);
+
+u_ref = u;
+H_ref = H;
 %% Hack for m=1
+%%
+pertubation = 0.01;
+lWin = 900e3;
+rWin = 1000e3;
 C = C.*abs(u).^(m-1);
+dC = pertubation.*C.*((x>=lWin)&(x<=rWin));
 m = 1;
 %% Solve SSA GL problem
 for i = 1: N_restart
-    [glInd, H, u, beta]=FlowlineSSA(H, b, x, dx, Nx, A, C, m, n, rhoi, rhow, g, as, dt, dt, u);
+    [glInd, H, u, beta]=FlowlineSSA(H, b, x, dx, Nx, A, C, 1, n, rhoi, ...
+        rhow, g, as, dt, dt, u);
     H_mat(:, i) = H;
     u_mat(:, i) = u;
+    beta_mat(:, i) = beta;
     gpos_vec(i) = glInd;
 end
-%% For adjSSA you need the input
-rhoig = rhoi*g;
-sigma= 1e2; 
-ist = 700;
-n = 3;
+
 
 %% Solve backward in time
 psi_old = zeros(N+1, 1);
 phi_old = zeros(N+1, 1);
 % Artificial viscosity
 epsilon = 1e3;
-Ascale = 1e0;
-for i =  N_restart:-1:1
-    % get the forward solutions
-    u = u_mat(:,i);
-    H = H_mat(:,i);
-    glInd = gpos_vec(i);
-    % set for adjoint
-    xAdj = x(2:glInd);
-    Nx = length(xAdj)+1;
-    I = eye(Nx-1);
-    %
-    u = u(2:glInd);
-    % H on stagger grid
-    H = (H(1:glInd-1)+H(2:glInd)) * 0.5;
-    % Beta
-    Cbeta = beta(2:glInd);
-    
-    % construct Adjoint matrices
-    [A11, A12, A21, A22, F1, F2, ux, eta, bb]=constrauctAdjSSAMatrices(...
-        Nx-1,n,ist,sigma,u,H,mean(bxc),A,rhoig,dx, uObs, HObs, glInd, epsilon, Cbeta, m, 0, transientFlag);
-    % Time stepping
-    Q = [A11*Ascale - transientFlag*Ascale./dt .* I,	A12;
-        A21*Ascale,                A22;];
-    rhs = [F1 - transientFlag*Ascale./dt .* psi_old(1:glInd-1);
-        F2;];
 
-    psifi = Q\rhs;
-    psi_old = psifi(1:Nx-1)*Ascale;
-    phi_old = psifi(Nx:2*Nx-2);    
-   
-    wght = -phi_old .* u.^m;
-    bwght = (Dm(Nx-1, dx)*psi_old).*u + (Dm(Nx-1,dx)* phi_old) .*eta .* ux+ rhoig*phi_old.*(Dm(Nx-1,dx)*H + bxc(1:Nx-1));
-%     bwght = rhoig*(H.*(Dm(Nx-1,dx)*phi_old) + phi_old.*(Dm(Nx-1,dx)*H + bxc(1:Nx-1))) +F1;
+for i =  1:Nist    
+    for t =  N_restart:-1:1
+        % get the forward solutions
+        u = u_mat(:,t);
+        H = H_mat(:,t);
+        glInd = gpos_vec(t);
+        % set for adjoint
+        xAdj = x(2:glInd);
+        Nx = length(xAdj)+1;
+        I = eye(Nx-1);
+        % forward solution
+        u = u(2:glInd);
+        % H on stagger grid
+        H = (H(1:glInd-1)+H(2:glInd)) * 0.5;
+        %
+        Cbeta = beta(2:glInd);
         
-    figure
-    subplot(4,1,1)
-    plot(xAdj, psi_old)
-    ylabel('$\psi$', 'Interpreter','latex')
-    subplot(4,1,2)
-    plot(xAdj, phi_old)
-    ylabel('$\phi$', 'Interpreter','latex')
-    subplot(4,1,3)
-    plot(xAdj, wght)
-    ylabel('$\delta\beta$', 'Interpreter','latex')
-    subplot(4,1,4)
-    plot(xAdj, bwght )
+        % construct Adjoint matrices
+        [A11, A12, A21, A22, F1, F2, ux, eta]=constrauctAdjSSAMatrices(...
+            Nx-1, n, ist(i), sigma, u, H, mean(bxc), A, rhoig, dx, uObs, HObs, glInd, epsilon, Cbeta, m, MacayealFalg, transientFlag);
+        % Time stepping
+        if t < (N_restart - 1/dt)
+            F1 = 0*F1;
+            F2 = 0*F2;
+        end
+        Q = [A11 - 1./dt .* I,	A12;
+            A21,                                A22;];
+        rhs = [F1 - 1./dt .* psi_old(1:glInd-1);
+            F2;];
+        % solve
+        psifi = Q\rhs;
+        psi = psifi(1:Nx-1);
+        phi = psifi(Nx:2*Nx-2);
+        
+        % sensitivity
+        wght = -phi .* u.^m;
+        bwght = (Dm(Nx-1, dx)*psi).*u + (Dm(Nx-1,dx)* phi) .*eta .* ux+ rhoig*phi.*(Dm(Nx-1,dx)*H + bxc(1:Nx-1));
+        
+        % save sensitivities
+        psi_mat(1:glInd-1, t, i) = psi;
+        psi_old = psi;
+        phi_mat(1:glInd-1, t, i) = phi;
+        wght_mat(1:glInd-1, t, i) = wght;
+        bwght_mat(1:glInd-1, t, i) = bwght;
+        
+        
+        % solve steady state SSA using Macayeal's formulation
+        % only valid for u observation
+        v = A22 \ F2;
+        v_mat(1:glInd-1, t, i) = v;
+        wght = -v .* u.^m;
+        vWght_mat(1:glInd-1, t, i) = wght;
+    end
+end
+%% save to data files
+if saveFlag
+    if uObs
+        obsName = 'u';
+    else
+        obsName = 'H';
+    end
+    
+    if transientFlag
+        if MacayealFalg
+            save(['DATA/SSA_Macayeal_Adjoint_N', num2str(N), '_T', num2str(N_restart), '_', obsName ,'.mat'], ...
+                'x', 'ist', 'dt', 'N_restart', 'psi_mat', 'phi_mat', 'wght_mat', 'bwght_mat', 'v_mat', 'vWght_mat');
+            
+        else
+            save(['DATA/SSAAdjoint_N', num2str(N), '_T', num2str(N_restart), '_', obsName ,'.mat'], ...
+                'x', 'ist', 'dt', 'N_restart', 'psi_mat', 'phi_mat', 'wght_mat', 'bwght_mat');
+        end
+        
+    else
+        if MacayealFalg
+            save(['DATA/SSA_Macayeal_Adjoint_N', num2str(N), '_', obsName , ...
+                '.mat'], 'x', 'ist', 'dt', 'N_restart', 'psi_mat', ...
+                'phi_mat', 'wght_mat', 'bwght_mat', 'v_mat', 'vWght_mat');
+        else
+            save(['DATA/SSAAdjoint_N', num2str(N), '_',obsName , '.mat'], ...
+                'x', 'ist', 'dt', 'N_restart', 'psi_mat', 'phi_mat', 'wght_mat', 'bwght_mat');
+        end
+    end
 end
 
-%%
-pert = sum(wght.*C(1:glInd-1)*0.01)*dx;
 
-%%
+%% Plot
+close all 
 
-t1 = rhoig*(H.*(Dm(Nx-1,dx)*phi_old));
-t2 = rhoig* phi_old.*(Dm(Nx-1,dx)*H + bxc(1:Nx-1)) ;
-t3 = F1;
-% t4 = rhoig*phi_old.*(bxc(1:Nx-1));
+tMesh = linspace(0, N_restart*dt, N_restart);
+[Xm, Tm] = meshgrid(x(2:end), tMesh);
+colormap(jet)
+
 figure
-% range1 = [-0.8e-7,0.8e-7];
-% range1 = [-0.2e-5,0.2e-5];
-subplot(3,1,1)
-plot(t1)
-% ylim(range1)
-subplot(3,1,2)
-plot(t2)
-% ylim(range1)
-subplot(3,1,3)
-plot(t3)
-% ylim(range1)
-% subplot(4,1,4)
-% plot(t4)
-% ylim(range1)
-%% b weights
-% 
-% t1 = (Dp(Nx-1, dx)* psi_old) .*u;
-% t2 = (Dp(Nx-1, dx)* phi_old) .*eta .* ux;
-% t3 = rhoig*phi_old.*(Dp(Nx-1,dx)*H + bxc(1:Nx-1));
-% figure
-% subplot(3,1,1)
-% plot(t1)
-% subplot(3,1,2)
-% plot(t2)
-% subplot(3,1,3)
-% plot(t3)
-%%
-% range2 = [-0.5e-4,0.5e-4];
-% range2 = [-2e-6,2e-6];
-% 
-% t1 = Dp(Nx-1,dx)*(1./n.*H.*eta.* (Dm(Nx-1,dx)*phi_old));
-% t2 =-m*bb.*phi_old;
-% t3 = -H.*(Dp(Nx-1,dx)*psi_old);
-% figure
-% subplot(3,1,1)
-% plot(t1)
-% ylim(range2)
-% subplot(3,1,2)
-% plot(t2)
-% ylim(range2)
-% subplot(3,1,3)
-% plot(t3)
-% ylim(range2)
-% 
-% %%
-% figure
-% % plot(u./eta./H.*(Dp(Nx-1,dx)*(eta.*H) ))
-% hold on
-% % plot(eta.*ux)
-% % plot(-rhog.*H)
-% % hold on
-% plot(1./(-rhog.*H))
-% plot(phi_old)
+colormap(jet)
+
+subplot(2, 2, 1)
+imagesc(x(2:end), tMesh, psi_mat')
+xlim([0, x(GLpos)])
+xlabel('x')
+ylabel('t')
+title('psi')
+colorbar
+
+subplot(2, 2, 2)
+imagesc(x(2:end), tMesh, ((phi_mat')))
+xlim([0, x(GLpos)])
+xlabel('x')
+ylabel('t')
+title('v')
+colorbar
+
+subplot(2, 2, 3)
+imagesc(x(2:end), tMesh, wght_mat')
+xlim([0, x(GLpos)])
+xlabel('x')
+ylabel('t')
+title('dC weights')
+colorbar
+
+subplot(2, 2, 4)
+imagesc(x(2:end), tMesh, bwght_mat')
+xlim([0, x(GLpos)])
+xlabel('x')
+ylabel('t')
+title('db weights')
+colorbar
+
+%% slice Plot along x=x(ist)
+figure
+
+subplot(2, 2, 1)
+plot(tMesh, psi_mat(ist+1,:))
+hold on
+plot(tMesh, psi_mat(ist,:))
+plot(tMesh, psi_mat(ist+2,:))
+xlabel('t')
+ylabel('psi')
+legend({'x(j)', 'x(j-1)', 'x(j+1)'}, 'location', 'northwest')
+
+subplot(2, 2, 2)
+plot(tMesh, phi_mat(ist+1,:))
+hold on
+plot(tMesh, phi_mat(ist,:))
+plot(tMesh, phi_mat(ist+2,:))
+xlabel('t')
+ylabel('v')
+legend({'x(j)', 'x(j-1)', 'x(j+1)'}, 'location', 'northwest')
+
+subplot(2, 2, 3)
+plot(tMesh, wght_mat(ist+1,:))
+hold on
+plot(tMesh, wght_mat(ist,:))
+plot(tMesh, wght_mat(ist+2,:))
+xlabel('t')
+ylabel('dC weights')
+legend({'x(j)', 'x(j-1)', 'x(j+1)'}, 'location', 'northwest')
+
+subplot(2, 2, 4)
+plot(tMesh, bwght_mat(ist+1,:))
+hold on
+plot(tMesh, bwght_mat(ist,:))
+plot(tMesh, bwght_mat(ist+2,:))
+xlabel('t')
+ylabel('db weights')
+legend({'x(j)', 'x(j-1)', 'x(j+1)'}, 'location', 'northwest')
+
