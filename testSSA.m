75486964158ad08f34170f75e50098c5fc1d12ea
Cheng Gong <cheng.gong@it.uu.se>
2020-01-08 15:14:53 +0100

    add time dependent adjoint and seasonal variations

--- testSSA.m
+++ testSSA.m
@@ -6,10 +6,10 @@ N = 800;
 
 %% Load final H and u from init file
 load(['DATA/SSAinit_N', num2str(N), '.mat'])
-saveFlag = 1;
+saveFlag = 0;
 
 %% Setup restart
-uObs = 1;
+uObs = 0;
 HObs = 1 - uObs;
 transientFlag = 1;
 MacayealFalg = 0;
@@ -19,23 +19,35 @@ rhoig = rhoi*g;
 sigma= 1e2;
 ist = [450];
 n = 3;
+dt = 0.05;
+obsT = 2*dt; % time period of the observation, from final time backward
 
 %% Pertubation in time
 N_restart = 400;
-dt = 0.05;
+T_final = dt * N_restart;
 sInd = [0: (N_restart-1)];
-seasonType = 0;
+seasonType = 1;
 
 if seasonType == 0
+    season = 0 * sInd;
+elseif seasonType == 1
     % sine [-1,1]
     season = sin(sInd*dt*2*pi);
-elseif seasonType == 1
+elseif seasonType == 2
     % cosine [-1,1]
     season = cos(sInd*dt*2*pi);
-else
+elseif seasonType == 3
     % square [0, 1]
     season = (sin(sInd*dt*2*pi) > 0)*2-1;
 end
+
+%% Hack for m=1
+pertubation = 0.1;
+lWin = x(1);
+rWin = x(end);
+C = C.*abs(u).^(m-1);
+dC = pertubation.*C.*((x>=lWin)&(x<=rWin));
+m = 1;
 
 %% Initialization
 Nist = length(ist);
@@ -50,22 +62,17 @@ phi_mat = zeros(N+1, N_restart, Nist);
 wght_mat = zeros(N+1, N_restart, Nist);
 bwght_mat = zeros(N+1, N_restart, Nist);
 
-v_mat = zeros(N+1, N_restart, Nist);
-vWght_mat = zeros(N+1, N_restart, Nist);
+if MacayealFalg
+    v_mat = zeros(N+1, N_restart, Nist);
+    vWght_mat = zeros(N+1, N_restart, Nist);
+end
 
 u_ref = u;
 H_ref = H;
-%% Hack for m=1
-%%
-pertubation = 0.01;
-lWin = 900e3;
-rWin = 1000e3;
-C = C.*abs(u).^(m-1);
-dC = pertubation.*C.*((x>=lWin)&(x<=rWin));
-m = 1;
+
 %% Solve SSA GL problem
 for i = 1: N_restart
-    [glInd, H, u, beta]=FlowlineSSA(H, b, x, dx, Nx, A, C, 1, n, rhoi, ...
+    [glInd, H, u, beta]=FlowlineSSA(H, b, x, dx, Nx, A, C+dC*season(i), 1, n, rhoi, ...
         rhow, g, as, dt, dt, u);
     H_mat(:, i) = H;
     u_mat(:, i) = u;
@@ -73,19 +80,20 @@ for i = 1: N_restart
     gpos_vec(i) = glInd;
 end
 
-
 %% Solve backward in time
 psi_old = zeros(N+1, 1);
 phi_old = zeros(N+1, 1);
 % Artificial viscosity
 epsilon = 1e3;
 
-for i =  1:Nist    
+for i =  1:Nist
     for t =  N_restart:-1:1
         % get the forward solutions
         u = u_mat(:,t);
         H = H_mat(:,t);
         glInd = gpos_vec(t);
+        Cbeta = beta_mat(2:glInd, t);
+        
         % set for adjoint
         xAdj = x(2:glInd);
         Nx = length(xAdj)+1;
@@ -94,21 +102,25 @@ for i =  1:Nist
         u = u(2:glInd);
         % H on stagger grid
         H = (H(1:glInd-1)+H(2:glInd)) * 0.5;
-        %
-        Cbeta = beta(2:glInd);
         
         % construct Adjoint matrices
         [A11, A12, A21, A22, F1, F2, ux, eta]=constrauctAdjSSAMatrices(...
-            Nx-1, n, ist(i), sigma, u, H, mean(bxc), A, rhoig, dx, uObs, HObs, glInd, epsilon, Cbeta, m, MacayealFalg, transientFlag);
-        % Time stepping
-        if t < (N_restart - 1/dt)
+            Nx-1, n, ist(i), sigma, u, H, mean(bxc), A, rhoig, dx, uObs, ...
+            HObs, glInd, epsilon, Cbeta, m, MacayealFalg, transientFlag);
+        
+        % Time dependent observation
+        if t <= (N_restart - obsT/dt)
             F1 = 0*F1;
             F2 = 0*F2;
         end
-        Q = [A11 - 1./dt .* I,	A12;
-            A21,                                A22;];
+        
+        % discrete system
+        Q = [A11 - 1./dt .* I,      A12;
+            A21,                    A22;];
+        
         rhs = [F1 - 1./dt .* psi_old(1:glInd-1);
             F2;];
+        
         % solve
         psifi = Q\rhs;
         psi = psifi(1:Nx-1);
@@ -128,10 +140,12 @@ for i =  1:Nist
         
         % solve steady state SSA using Macayeal's formulation
         % only valid for u observation
-        v = A22 \ F2;
-        v_mat(1:glInd-1, t, i) = v;
-        wght = -v .* u.^m;
-        vWght_mat(1:glInd-1, t, i) = wght;
+        if MacayealFalg
+            v = A22 \ F2;
+            v_mat(1:glInd-1, t, i) = v;
+            wght = -v .* u.^m;
+            vWght_mat(1:glInd-1, t, i) = wght;
+        end
     end
 end
 %% save to data files
@@ -166,11 +180,10 @@ end
 
 
 %% Plot
-close all 
+close all
 
 tMesh = linspace(0, N_restart*dt, N_restart);
 [Xm, Tm] = meshgrid(x(2:end), tMesh);
-colormap(jet)
 
 figure
 colormap(jet)
